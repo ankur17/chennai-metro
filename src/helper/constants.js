81c1657f1d2745752283b053abf90760ae980869
ankur17 <anankur17@gmail.com>
2019-06-06 18:22:38 +0530

    adjacent data added

--- src/helper/constants.js
+++ src/helper/constants.js
@@ -7,38 +7,38 @@ export const cards = ["Commutatus","USA consulate","Music Academy","Marina","Exp
 export const stationDetails = {
     commutatus : {
         name : "Commutatus",
-        ajdacent : [{name : "USA consulate",distace : 8},{name : "Sathyam Cinemas",distace : 16}]
+        adjacent : [{name : "USA consulate",distance : 8},{name : "Sathyam Cinemas",distance : 16}]
     },
     usa_consulate : {
         name : "USA consulate",
-        ajdacent : [{name : "USA consulate",distace : 8},{name : "Sathyam Cinemas",distace : 16}]
+        adjacent : [{name : "USA consulate",distance : 8},{name : "Sathyam Cinemas",distance : 16}]
     },
     music_academy : {
         name : "Music Academy",
-        ajdacent : []
+        adjacent : [{name : "USA consulate",distance : 16},{name : "MA Chidambaram Stadium",distance : 4},{name : "Central Railway station",distance : 8}]
     },
     marina : {
         name : "Marina",
-        ajdacent : []
+        adjacent : [{name : "MA Chidambaram Stadium",distance : 14},{name : "Express Avenue Mall",distance : 18}]
     },
     express_avenue_mall : {
         name : "Express Avenue Mall",
-        ajdacent : []
+        adjacent : [{name : "Central Railway station",distance : 20},{ name : "MA Chidambaram Stadium",distance : 18}]
     },
     Central_railway_station : {
         name : "Central Railway station",
-        ajdacent : []
+        adjacent : [{name : "Express Avenue Mall",distance:20}, {name : "MA Chidambaram Stadium",distance : 28}]
     },
     Government_museum : {
         name : "Government Museum",
-        ajdacent : []
+        adjacent : [{name : "Sathyam Cinemas",distance:2},{name : "MA Chidambaram Stadium",distance:4},{name : "Marina",distance:12}]
     },
     sathyam_cinemas : {
         name : "Sathyam Cinemas",
-        ajdacent : []
+        adjacent : [{name : "MA Chidambaram Stadium",distance:14},{name : "Government Museum",distance:16},{name : "Commutatus",distance:22},{name : "MA Chidambaram Stadium",distance:14}]
     },
     ma_Chidambaram_stadium : {
         name : "MA Chidambaram Stadium",
-        ajdacent : []
+        adjacent : [{name : "Government Museum",distance:12},{name : "Marina",distance:14},{name : "Sathyam Cinemas",distance:14},{name : "Music Academy",distance:4}]
     }
 }
